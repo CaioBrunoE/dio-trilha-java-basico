48956b93c43e1919c326df27e6cb5b06ed27dac2
Caio Bruno <123472004+CaioBrunoE@users.noreply.github.com>
2024-08-28 10:57:39 -0300

    Completando dessafio da dio conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        Integer numero;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+
+        System.out.println("Qual o seu nome !");
+        nomeCliente =  sc.nextLine();
+
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia =  sc.nextLine();
+
+
+        System.out.println("Número da conta !");
+        numero =  sc.nextInt();
+
+        System.out.println("Qual seu saldo !");
+        saldo =  sc.nextDouble();
+
+
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
+                nomeCliente,agencia,numero, saldo );
+
+        sc.close();
     }
 }
